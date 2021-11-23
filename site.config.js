0c4e10a500392620fb352d7dc4508d89dec333f3
zansgit <94211252+zansgit@users.noreply.github.com>
2021-11-23 21:14:43 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'ZAN-739833b1dc7c470bbb891da92e87b761',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
